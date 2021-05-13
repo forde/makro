7003065485fa8b5e64e2e6d98851916e91177bcc
Konrad <konrad@forde.pl>
2021-05-13 23:49:09 +0200

    firebase setup

--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,22 @@ function HomePage() {
 
     // set up collection state listener
     useEffect(() => {
-        if(!firebase) return
-        firebase.firestore().collection('collection').onSnapshot(snap => {
+        /*if(!firebase) return
+        firebase.firestore().collection('ingredients').onSnapshot(snap => {
             let data = {}
-            snap.forEach((doc) => data[doc.id] = doc.data()?.value )
+            snap.forEach((doc) => data[doc.id] = doc.data() )
             console.log('New data snapshot', data)
-        })
+        })*/
+    }, [firebase])
+
+    // get data once
+    useEffect(() => {
+        if(!firebase) return
+        firebase.firestore().collection('ingredients').get().then(snap => {
+            let data = {}
+            snap.forEach((doc) => data[doc.id] = doc.data() )
+            console.log('Data snapshot', data)
+        }).catch(e => console.log(e))
     }, [firebase])
 
     const signIn = () => {
@@ -40,7 +50,11 @@ function HomePage() {
 
     // firebase.firestore().collection('cacheKeys').doc('persistorKey').set({ value: cacheKeys.persistorKey + 1}, { merge: true })
 
-    return <div>Welcome to Next.js!</div>
+    return(
+        <div>
+            <h1>Makro</h1>
+        </div>
+    )
 }
   
 export default HomePage
