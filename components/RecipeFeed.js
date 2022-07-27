dfc3e644931f87137bf5cb53d44ee4d325b8b1e2
konrad <konrad@kodebyraaet.no>
2022-07-27 12:46:53 +0200

    minor layout chnages

--- components/RecipeFeed.js
+++ components/RecipeFeed.js
@@ -1,25 +1,50 @@
-import { truncate } from '~/lib/helpers'
 import Link from 'next/link'
-import Image from 'next/image'
 import RecipeMakro from '~/components/RecipeMakro'
 import { imagekitUrl } from '~/lib/helpers'
+import { styled } from '@stitches/react'
 
 export default function RecipeFeed({ recipes, admin }) {
-    return (recipes || []).map(rec => <RecipeItem recipe={rec} key={rec.slug} admin={admin} />)
+    return (
+        <Wrapper>
+            {(recipes || []).map(rec => <RecipeItem recipe={rec} key={rec.slug} admin={admin} />)}
+        </Wrapper>
+    )
 }
 
 function RecipeItem({ recipe, admin }) {
     return(
         <Link href={`/[username]/[slug]`} as={`/${recipe.username}/${recipe.slug}`}>
             <a>
-                <div className="card p-16 mb-32">
+                <div className="card pl-16 pr-16 pt-16">
                     <div className="br-10 oh mb-24">
                         <img src={imagekitUrl(recipe?.thumbnail, 'tr=w-728')} />
                     </div>
-                    <h1 className="bold mb-24 pl-16 pr-16">{recipe.title}</h1>
-                    <RecipeMakro recipe={recipe} perPortion className="pl-16 pr-16"/>
+                    <h2 className="bold mb-24">{recipe.title}</h2>
+                    <RecipeMakro recipe={recipe} perPortion justIcons />
                 </div>
             </a>
         </Link>
     )
 }
+
+const Wrapper = styled('div', {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginLeft: '-10px',
+    marginRight: '-10px',
+    '> a': {
+        width: 'calc(50% - 20px)',
+        margin: '0 10px 20px 10px'
+    },
+    '.card': {
+        paddingBottom: '8px'
+    },
+    '@media(max-width: 767px)': {
+        marginLeft: '0px',
+        marginRight: '0px',
+        '> a': {
+            width: '100%',
+            margin: '0 0 20px 0'
+        },
+    }
+})
